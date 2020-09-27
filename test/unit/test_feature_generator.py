546fe57f079a7547232d51b073c172b55c52ed17
Rahul Yadav <rahul.z.yadav@here.com>
2020-09-27 15:35:36 +0200

    Add tests for feature generator

---
+++ test/unit/test_feature_generator.py
@@ -0,0 +1,6 @@
+from ...utils import feature_generator
+
+import pytest
+
+def test_generate_features():
+    pass
---
+++ utils/request_validator.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def generate_features(df: pd.DataFrame)->pd.DataFrame:
+    pass
