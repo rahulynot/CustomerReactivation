1b5440c01772f5f639328509ac2b2d480a662d34
Rahul Yadav <rahul.z.yadav@here.com>
2020-09-27 18:13:11 +0200

    Is_valid complete implementation

--- test/unit/test_request_converter.py
+++ test/unit/test_request_converter.py
@@ -1,15 +1,15 @@
+from datetime import datetime
+
 import pytest
 
-from ...utils.request_converter import RequestConverter
-
-sample_request = {
-    "customer_id": 123,  # customer id
-    "country_code": "Peru",  # customer’s country
-    "last_order_ts": "2018-05-03 00:00:00",  # ts of the last order placed by a customer
-    "first_order_ts": "2017-05-03 00:00:00",  # ts of the first order placed by a customer
-    "total_orders": 15,  # total orders placed by a customer
-    "segment_name": "recency_segment",  # which segment a customer belongs to
-}
+from ...utils.common import Segment
+from ...utils.request_converter import (
+    COUNTRY_CODE_FIELD,
+    LAST_ORDER_TS_FIELD,
+    SEGMENT_NAME_FIELD,
+    TOTAL_ORDER_FIELD,
+    RequestConverter,
+)
 
 
 def test_is_valid_fails_for_invalid_country_code_key():
@@ -34,7 +34,7 @@ def test_is_valid_fails_for_invalid_country_code_value():
 
     with pytest.raises(ValueError) as excinfo:
         req_conv.is_valid()
-    assert "Invalid  country_code Japan in the request" in str(excinfo.value)
+    assert f"Invalid  {COUNTRY_CODE_FIELD} Japan" in str(excinfo.value)
 
 
 def test_is_valid_fails_for_invalid_last_order_ts_key():
@@ -52,7 +52,7 @@ def test_is_valid_fails_for_invalid_last_order_ts_key():
 
         with pytest.raises(KeyError) as excinfo:
             req_conv.is_valid()
-        assert "Missing/Incorrect field last_order_ts in the request" in str(excinfo.value)
+        assert f"Missing/Incorrect field {LAST_ORDER_TS_FIELD} in the request" in str(excinfo.value)
 
 
 def test_is_valid_fails_for_invalid_last_order_ts_value():
@@ -73,6 +73,114 @@ def test_is_valid_fails_for_invalid_last_order_ts_value():
         assert f"Invalid last_order_ts {request['last_order_ts']} in the request" in str(
             excinfo.value
         )
+
+
+def test_is_valid_fails_for_invalid_total_orders_key():
+
+    requests = [
+        # Missing total_orders value
+        {"customer_id": 123, "country_code": "Peru", "last_order_ts": "2018-05-03 00:00:00",},
+        # Incorrect key name for total_orders
+        {
+            "customer_id": 123,
+            "country_code": "Peru",
+            "last_order_ts": "2018-05-03 00:00:00",
+            "order_totals": 15,
+        },
+    ]
+
+    for request in requests:
+
+        req_conv = RequestConverter(request)
+
+        with pytest.raises(KeyError) as excinfo:
+            req_conv.is_valid()
+        assert f"Missing/Incorrect field {TOTAL_ORDER_FIELD} in the request" in str(excinfo.value)
+
+
+def test_is_valid_fails_for_invalid_total_orders_value():
+
+    request = {
+        "customer_id": 123,
+        "country_code": "Peru",
+        "last_order_ts": "2018-05-03 00:00:00",
+        "total_orders": "invalid-value",
+    }
+
+    req_conv = RequestConverter(request)
+
+    with pytest.raises(ValueError) as excinfo:
+        req_conv.is_valid()
+    assert f"Invalid {TOTAL_ORDER_FIELD} invalid-value in the request" in str(excinfo.value)
+
+
+def test_is_valid_fails_for_invalid_segment_name_key():
+
+    requests = [
+        # Missing segment_nam value
+        {
+            "customer_id": 123,
+            "country_code": "Peru",
+            "last_order_ts": "2018-05-03 00:00:00",
+            "total_orders": 15,
+        },
+        # Incorrect key name for total_orders
+        {
+            "customer_id": 123,
+            "country_code": "Peru",
+            "last_order_ts": "2018-05-03 00:00:00",
+            "total_orders": 15,
+            "some_segment": "",
+        },
+    ]
+
+    for request in requests:
+
+        req_conv = RequestConverter(request)
+
+        with pytest.raises(KeyError) as excinfo:
+            req_conv.is_valid()
+        assert f"Missing/Incorrect field {SEGMENT_NAME_FIELD} in the request" in str(excinfo.value)
+
+
+def test_is_valid_fails_for_invalid_segment_name_value():
+
+    request = {
+        "customer_id": 123,
+        "country_code": "Peru",
+        "last_order_ts": "2018-05-03 00:00:00",
+        "total_orders": 15,
+        "segment_name": "invalid_segment",
+    }
+
+    req_conv = RequestConverter(request)
+
+    with pytest.raises(ValueError) as excinfo:
+        req_conv.is_valid()
+    assert f"Invalid {SEGMENT_NAME_FIELD} invalid_segment" in str(excinfo.value)
+
+
+def test_is_valid_succeeds_for_valid_request():
+
+    valid_request = {
+        "customer_id": 123,  # customer id
+        "country_code": "Peru",  # customer’s country
+        "last_order_ts": "2018-05-03 00:00:00",  # ts of the last order placed by a customer
+        "first_order_ts": "2017-05-03 00:00:00",  # ts of the first order placed by a customer
+        "total_orders": 15,  # total orders placed by a customer
+        "segment_name": "recency_segment",  # which segment a customer belongs to
+    }
+
+    req_conv = RequestConverter(valid_request)
+
+    assert req_conv.is_valid() == True
+    assert req_conv.country_code == "Peru"
+    assert (
+        req_conv.last_order_ts
+        == datetime.strptime(valid_request[LAST_ORDER_TS_FIELD], "%Y-%m-%d %H:%M:%S").date()
+    )
+    assert req_conv.total_orders == 15
+    assert req_conv.segment_name == Segment.RECENCY
 
 
 def test_convert_request():
--- utils/request_converter.py
+++ utils/request_converter.py
@@ -1,9 +1,22 @@
 import pandas as pd
 from datetime import datetime
+from .common import Segment
 
 VALID_COUNTRY_CODES = ["Australia", "China", "Latvia", "Peru"]
 COUNTRY_CODE_FIELD = "country_code"
 LAST_ORDER_TS_FIELD = "last_order_ts"
+TOTAL_ORDER_FIELD = "total_orders"
+SEGMENT_NAME_FIELD = "segment_name"
+
+
+def convert_segment_name(segment_name: str) -> Segment:
+
+    if segment_name == "recency_segment":
+        return Segment.RECENCY
+    elif segment_name == "frequent_segment":
+        return Segment.FREQUENT
+
+    raise ValueError(f"Invalid {SEGMENT_NAME_FIELD} {segment_name}")
 
 
 class RequestConverter:
@@ -11,8 +24,12 @@ class RequestConverter:
         self.request = request
         self.country_code = None
         self.last_order_ts = None
+        self.total_orders = None
+        self.segment_name = None
+        self.current_ts = datetime.now().date()
 
     def is_valid(self) -> bool:
+        # Parse 'country_code' field
         if COUNTRY_CODE_FIELD not in self.request:
             raise KeyError(f"Missing/Incorrect field {COUNTRY_CODE_FIELD} in the request")
 
@@ -23,19 +40,40 @@ class RequestConverter:
         else:
             self.country_code = self.request[COUNTRY_CODE_FIELD]
 
+        # Parse 'last_order_ts' field
         if LAST_ORDER_TS_FIELD not in self.request:
             raise KeyError(f"Missing/Incorrect field {LAST_ORDER_TS_FIELD} in the request")
-
-        # Parse 'last_order_ts' field
         else:
             try:
                 self.last_order_ts = datetime.strptime(
                     self.request[LAST_ORDER_TS_FIELD], "%Y-%m-%d %H:%M:%S"
-                )
+                ).date()
             except ValueError:
                 raise ValueError(
                     f"Invalid {LAST_ORDER_TS_FIELD} {self.request[LAST_ORDER_TS_FIELD]} in the request"
                 )
+
+        # Parse 'total_orders' field
+        if TOTAL_ORDER_FIELD not in self.request:
+            raise KeyError(f"Missing/Incorrect field {TOTAL_ORDER_FIELD} in the request")
+        else:
+            if type(self.request[TOTAL_ORDER_FIELD]) != int:
+                raise ValueError(
+                    f"Invalid {TOTAL_ORDER_FIELD} {self.request[TOTAL_ORDER_FIELD]} in the request"
+                )
+
+            self.total_orders = int(self.request[TOTAL_ORDER_FIELD])
+
+        # Parse 'segment_name' field
+        if SEGMENT_NAME_FIELD not in self.request:
+            raise KeyError(f"Missing/Incorrect field {SEGMENT_NAME_FIELD} in the request")
+        else:
+            try:
+                self.segment_name = convert_segment_name(self.request[SEGMENT_NAME_FIELD])
+            except ValueError as e:
+                raise ValueError(e)
+
+        return True
 
     def convert(self) -> pd.DataFrame:
         pass
